4b0f4aa6eeae6c706228658624e713a77f5951fe
elaleman01 <115910181+elaleman01@users.noreply.github.com>
2025-01-13 22:53:44 -0300

    Update dns.tf

--- dns.tf
+++ dns.tf
@@ -9,7 +9,7 @@ terraform {
 provider "fortios" {
 hostname = "192.168.203.1"
 token = "r5bm6wbhw9kc4dzpGkz8xdcbb453hk"
-insecure = "false"
+insecure = "true"
 }
 resource "fortios_system_setting_dns" "dns1" {
 primary = "4.2.2.2"
