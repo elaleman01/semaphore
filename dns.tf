fa9c090c7b18c3aa4100828db07e32e1688e37f8
elaleman01 <115910181+elaleman01@users.noreply.github.com>
2025-01-30 23:22:38 -0300

    Update dns.tf

--- dns.tf
+++ dns.tf
@@ -9,7 +9,7 @@ terraform {
 # Configure the FortiOS Provider
 provider "fortios" {
 hostname = "192.168.203.1"
-token = "r5bm6wbhw9kc4dzpGkz8xdcbb453hk"
+token = TF_VAR_api_token
 insecure = "true"
 }
 resource "fortios_system_setting_dns" "dns1" {
