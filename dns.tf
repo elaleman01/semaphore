75e39d506619d3d53670bc0afcd667c9c2ad8921
elaleman01 <115910181+elaleman01@users.noreply.github.com>
2025-02-04 10:01:50 -0300

    Update dns.tf

--- dns.tf
+++ dns.tf
@@ -10,14 +10,18 @@ variable "host" {
   type      = string
   sensitive = true
 }
+variable "token" {
+  type      = string
+  sensitive = true
+}
 
 # Configure the FortiOS Provider
 provider "fortios" {
 hostname = var.host
-token = "r5bm6wbhw9kc4dzpGkz8xdcbb453hk"
+token = var.token
 insecure = "true"
 }
 resource "fortios_system_setting_dns" "dns1" {
 primary = "8.8.8.8"
-secondary = "4.2.2.2"
+secondary = "8.8.1.1"
 }
